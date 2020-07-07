84b5e85b0428f59ff33fb6a8038ea2c724d4297f
vagrant <akifumi.morita@gmail.com>
2020-07-07 13:36:26 +0000

    [Update]教科書P123

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,28 +26,18 @@ $(function(){
     // 
     var duration = 300;
 
-    // buttons2 ----------------------------------------
-    $('#buttons2 button').each(function(index){
-        //var pos = Math.random() * 80 - 40;
-        var pos = index * 40 - 40;
-        $(this).css('top', pos);
-    })
-    .on('mouseover', function(){
-        var $btn = $(this).stop(true).animate({
-            backgroundColor: '#faee00',
-            color: '#000'
-        }, duration);
-        $btn.find('img:first-child').stop(true).animate({opacity: 0}, duration);
-        $btn.find('img:nth-child(2)').stop(true).animate({opacity: 1}, duration);
-    })
-    .on('mouseout', function(){
-        var $btn = $(this).stop(true).animate({
-            backgroundColor: '#fff',
-            color: '#01b169',
-        }, duration);
-        $btn.find('img:first-child').stop(true).animate({opacity: 1}, duration);
-        $btn.find('img:nth-child(2)').stop(true).animate({opacity: 0}, duration);
-    });
-
+    // aside ----------------------------------------
+    var $aside = $('.page-main > aside');
+    var $asidButton = $aside.find('button')
+        .on('click', function(){
+            $aside.toggleClass('open');
+            if($aside.hasClass('open')){
+                $aside.stop(true).animate({left: '-70px'}, duration, 'easeOutBack');
+                $asidButton.find('img').attr('src', '/assets/btn_close.png');
+            }else{
+                $aside.stop(true).animate({left: '-350px'}, duration, 'easeInBack');
+                $asidButton.find('img').attr('src', '/assets/btn_open.png');
+            };
+        });
 
 });
